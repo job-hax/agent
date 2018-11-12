73e2792b5b4e7c517495e607170f6a32fba1443d
Egemen Uslu <egemeny.uslu@gmail.com>
2018-11-11 17:36:56 -0800

    metrics page updated

--- src/errorcv/static/js/addnew.js
+++ src/errorcv/static/js/addnew.js
@@ -52,9 +52,9 @@ var CollectionCreateForm = Form.create()(/** @class */ (function (_super) {
                 })(React.createElement(Radio.Group, null,
                     React.createElement(Radio, { value: "1" }, "N/A"),
                     React.createElement(Radio, { value: "2" }, "Planning"),
-                    React.createElement(Radio, { value: "2" }, "In Progress"),
-                    React.createElement(Radio, { value: "3" }, "Offer"),
-                    React.createElement(Radio, { value: "4" }, "Fail")))),
+                    React.createElement(Radio, { value: "3" }, "In Progress"),
+                    React.createElement(Radio, { value: "4" }, "Offer"),
+                    React.createElement(Radio, { value: "5" }, "Fail")))),
                 React.createElement(FormItem, { label: "Source" }, getFieldDecorator('source', {
                     rules: [{ required: true, message: 'Please enter the source!' }]
                 })(React.createElement(Input, null))))));
